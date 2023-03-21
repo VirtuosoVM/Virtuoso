49852b2e1fa10ba8086457388d7715c129931d23
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2023-03-21 22:40:52 +0000

    uncomment sigterm handler

--- src/main.ts
+++ src/main.ts
@@ -236,18 +236,18 @@ pmx.action("test-bot", async function (reply: Function): Promise<void> {
 
 
 
-//function exitHandler(options, exitCode) {
-//    if (options.cleanup) {
-//        client.destroy();
-//    }
-//};
-//
-//
-//process.on("exit", exitHandler.bind(null, { cleanup: true }));
-//process.on("SIGINT", exitHandler.bind(null, { cleanup: true }));
-//process.on("SIGTERM", exitHandler.bind(null, { cleanup: true }));
-//process.on("SIGUSR1", exitHandler.bind(null, { cleanup: true }));
-//process.on("SIGUSR2", exitHandler.bind(null, { cleanup: true }));
+function exitHandler(options, exitCode) {
+    if (options.cleanup) {
+        client.destroy();
+    }
+}
+
+
+process.on("exit", exitHandler.bind(null, { cleanup: true }));
+process.on("SIGINT", exitHandler.bind(null, { cleanup: true }));
+process.on("SIGTERM", exitHandler.bind(null, { cleanup: true }));
+process.on("SIGUSR1", exitHandler.bind(null, { cleanup: true }));
+process.on("SIGUSR2", exitHandler.bind(null, { cleanup: true }));
 
 
 process.on("message", function (msg) {
