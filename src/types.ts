085e03cf26718dafadb300dd96efd23e0c7d2493
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2023-03-22 17:59:45 +0000

    config typing

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,7 @@
 import type * as Discord from "discord.js";
 import type * as VMRun from "vmrun";
 
+import type * as config from "../config.json";
 import type * as helper_funcs from "./helper_funcs";
 
 export type Int = number & { __int__: void };
@@ -9,13 +10,15 @@ export type Entries<T> = {
     [K in keyof T]: [K, T[K]];
 }[keyof T][];
 
+
+export type BotConfig = typeof config;
 export interface CommandCallData {
     Discord: typeof Discord,
     client: Discord.Client,
     args: string[],
     cased_args: string[],
     commands: { [key: string]: any },
-    config: { [key: string]: any },
+    config: BotConfig,
     booting_vms: string[],
     VMRun: typeof VMRun,
     helper_functions: typeof helper_funcs,
